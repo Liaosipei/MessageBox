165c4e9d13836e1d3a2a666416845f39e2a1ed25
liaosipei <liaosipei@foxmail.com>
2015-08-27 18:41:28 +0800

    Initial Commit

---
+++ MessageBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MessageBox
+//
+//  Created by liaosipei on 15/8/27.
+//  Copyright (c) 2015年 liaosipei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MessageBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MessageBox
+//
+//  Created by liaosipei on 15/8/27.
+//  Copyright (c) 2015年 liaosipei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
